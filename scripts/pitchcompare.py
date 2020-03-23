76d0ac79f2b752f6ed600d2285d2e4b8e1036913
Albert Gassol Puigjaner <albertgassol1@gmail.com>
2020-03-23 20:04:18 +0100

    Pitch amb python

---
+++ scripts/pitchcompare.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+file = open("../../pav_2341.f0", 'r')
+
+valuesf0 = []
+interval = 0.015
+time = 0.15
+
+for line in file.readlines():
+    valuesf0.append(float(line))
+    time+=interval
+file.close()
+
+file = open("../../pav_2341.f0ref", 'r')
+
+valuesf0ref = []
+time = 0.015
+for line in file.readlines():
+    valuesf0ref.append(float(line))
+    time+=interval
+
+file.close()
+
+fig, axs = plt.subplots(2)
+fig.suptitle('Pitch de calculado y pitch de referencia')
+axs[0].plot(valuesf0, 'ro', markersize = 2)
+axs[1].plot(valuesf0ref, 'bo', markersize = 2)
+plt.show()
